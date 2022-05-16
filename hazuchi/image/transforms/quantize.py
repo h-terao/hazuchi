26317843808afa88859218191f91303041533e58
h-terao <48378674+h-terao@users.noreply.github.com>
2022-05-16 16:34:17 +0900

    Update quantize.py

--- hazuchi/image/transforms/quantize.py
+++ hazuchi/image/transforms/quantize.py
@@ -4,9 +4,15 @@ import chex
 
 
 def quantize(img: chex.Array) -> chex.Array:
-    """Quantize pixel velues as integers.
-
-    Returns int(x * 255) / 255.
+    """Quantize pixel velues as integers. 
+       
+    Compute int(x * 255) / 255, and apply STE to be differentiable.
+    
+    Args:
+        img (array): An image array. Each pixel value is expected to be in [0,1].
+        
+    Returns:
+        Quantized image array. 
     """
     quantized = (255 * img).astype(jnp.uint8)
     quantized /= 255
