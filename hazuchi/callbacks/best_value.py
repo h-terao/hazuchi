2a3a4e0adffa3cd5bd6a951874fe0226647f35c7
Terao Hayato <terao@ist.hokudai.ac.jp>
2022-05-21 13:16:48 +0900

    update

--- hazuchi/callbacks/best_value.py
+++ hazuchi/callbacks/best_value.py
@@ -17,12 +17,13 @@ class BestValue(callback.Callback):
     priority: int = callback.PRIORITY_WRITER
 
     def __init__(self, name: str, monitor: str, mode: str = "min"):
+        assert mode in ["min", "max"]
         self.name = name
         self.monitor = monitor
         self.compare = operator.lt if mode == "min" else operator.gt
         self.best_score = math.inf if mode == "min" else -math.inf
 
     def on_fit_epoch_end(self, trainer, train_state, summary):
-        self.best_score = self.compare(self.best_score, summary.get(self.monitor, self.best_score))
+        self.best_score = self.compare(summary.get(self.monitor, self.best_score), self.best_score)
         summary[self.name] = self.best_score
         return train_state, summary
