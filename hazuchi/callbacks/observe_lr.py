cff82a3f7a0e3b274e085a12d43c5cbb0192dc7d
Terao Hayato <terao@ist.hokudai.ac.jp>
2022-05-22 09:45:25 +0900

    update

--- hazuchi/callbacks/observe_lr.py
+++ hazuchi/callbacks/observe_lr.py
@@ -1,6 +1,7 @@
 from __future__ import annotations
 from typing import Callable
 
+import jax
 import jax.numpy as jnp
 from . import callback
 
@@ -14,7 +15,7 @@ class ObserveLR(callback.Callback):
         if jnp.isscalar(learning_rate):
             self.learning_rate_fun = lambda _: learning_rate  # noqa: E731
         else:
-            self.learning_rate_fun = learning_rate
+            self.learning_rate_fun = jax.jit(learning_rate)
 
     def on_fit_epoch_end(self, trainer, train_state, summary):
         lr = self.learning_rate_fun(trainer.global_step)
--- hazuchi/utils/serialization.py
+++ hazuchi/utils/serialization.py
@@ -4,32 +4,8 @@ import pickle
 import gzip
 import uuid
 
-from flax.training.train_state import TrainState
-from flax.serialization import to_state_dict, from_state_dict
-from ..trainer import Trainer
-
 
 __all__ = ["load_state", "save_state"]
-
-
-# def save_checkpoint(file, trainer: Trainer, train_state: TrainState):
-#     checkpoint = {
-#         "trainer": trainer.to_state_dict(),
-#         "train_state": to_state_dict(train_state),
-#     }
-#     with open(file, "wb") as fp:
-#         pickle.dump(checkpoint, fp)
-
-
-# def load_checkpoint(
-#     file, trainer: Trainer, train_state: TrainState, only_train_state: bool = False
-# ) -> tuple[Trainer, TrainState]:
-#     with open(file, "rb") as fp:
-#         checkpoint = pickle.load(fp)
-#     if not only_train_state:
-#         trainer.from_state_dict(checkpoint["trainer"])
-#     train_state = from_state_dict(train_state, checkpoint["train_state"])
-#     return trainer, train_state
 
 
 def load_state(file) -> dict:
