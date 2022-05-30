2083a19aa653bae2b20eec0ffc8347c5a5e97d9a
Terao Hayato <terao@ist.hokudai.ac.jp>
2022-05-30 20:36:17 +0900

    update

--- hazuchi/utils/double_buffer.py
+++ hazuchi/utils/double_buffer.py
@@ -9,11 +9,12 @@ Batch = Mapping[str, np.ndarray]
 
 def _device_put_sharded(sharded_tree, devices):
     leaves, treedef = jax.tree_flatten(sharded_tree)
-    n = list(leaves)[0].shape[0]
-    return jax.device_put_sharded(
-        [jax.tree_unflatten(treedef, [l[i] for l in leaves]) for i in range(n)],
-        devices,
-    )
+    for leave in leaves:
+        n = leave.shape[0]
+        return jax.device_put_sharded(
+            [jax.tree_unflatten(treedef, [l[i] for l in leaves]) for i in range(n)],
+            devices,
+        )
 
 
 def double_buffer(ds: Iterable[Batch]) -> Iterator[Batch]:
