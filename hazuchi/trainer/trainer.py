5f24c0c40fe185d61e143ae47d75e4b4caa42ce5
h-terao <48378674+h-terao@users.noreply.github.com>
2022-06-26 14:19:23 +0900

    Update trainer.py

--- hazuchi/trainer/trainer.py
+++ hazuchi/trainer/trainer.py
@@ -79,6 +79,7 @@ class Trainer:
     def _loop_epoch(self, train_state, fn, iterator, prefix, train: bool, iter_len=None):
         accum_scalars = {}
         for batch, weight in split_batches(iterator, iter_len, self.prefetch, self.devices):
+            print(jax.tree_map(lambda x: x.shape, batch))
             if train:
                 train_state, scalars = fn(train_state, batch)
             else:
