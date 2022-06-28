36d44a08a08cf818b85b3525be748d2ef4b7139a
TERAO Hayato <terao@ist.hokudai.ac.jp>
2022-06-28 16:10:25 +0900

    update __init__.py

--- hazuchi/callbacks/__init__.py
+++ hazuchi/callbacks/__init__.py
@@ -1,8 +1,12 @@
 # flake8: noqa
 from .best_value import BestValue
 from .early_stopping import EarlyStopping
-from .json_logger import JsonLogger
 from .print_metrics import PrintMetrics
 from .progress_bar import ProgressBar
 from .snapshot import Snapshot
 from .timer import Timer
+
+# Loggers.
+from .json_logger import JsonLogger
+from .comet_logger import CometLogger
+from .wandb_logger import WandbLogger
